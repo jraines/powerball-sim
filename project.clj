eaa008ecd455c9bf98be019ee9c34b54ba8e09f3
Jeremy Raines <jraines@gmail.com>
2016-01-15 15:35:40 -0800

    Parinfer formatting tweaks

--- project.clj
+++ project.clj
@@ -42,7 +42,7 @@
              ;; :server-port 3449 ;; default
              ;; :server-ip "127.0.0.1"
 
-             :css-dirs ["resources/public/css"] ;; watch and update CSS
+             :css-dirs ["resources/public/css"]}) ;; watch and update CSS
 
              ;; Start an nREPL server into the running figwheel process
              ;; :nrepl-port 7888
@@ -67,4 +67,3 @@
 
              ;; to configure a different figwheel logfile path
              ;; :server-logfile "tmp/logs/figwheel-logfile.log"
-             })
